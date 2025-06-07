6d16c9bae9070eaecf9878a87b4e6a3097dfd3ef
Shark-Mateus-Monteiro <shark06816@sharkcoders.pt>
2025-06-07 10:33:29 +0100

    Add files via upload

---
+++ Aula N sei dnv/PlayerControls.cs
@@ -0,0 +1,51 @@
+using Godot;
+using System;
+
+public partial class PlayerControls : CharacterBody3D
+{
+	public int Speed = 30;
+	public int Gravity = 196;
+	public int Jump = 10;
+	private Vector3 _PlayerVelocity = Vector3.Zero;
+
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		var direction = Vector3.Zero;
+		Connect("body_entered", new Callable(this, nameof(OnBodyEntered)));
+
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		var direction = Vector3.Zero;
+
+		if (Input.IsActionPressed("ui_left")) direction.X -= 1.0f;
+		if (Input.IsActionPressed("ui_right")) direction.X += 1.0f;
+		if (Input.IsActionPressed("ui_up")) direction.Z -= 1.0f;
+		if (Input.IsActionPressed("ui_down")) direction.Z += 1.0f;
+		if (Input.IsActionPressed("ui_accept"))
+			_PlayerVelocity.Y = Jump * Speed;
+		direction = direction.Normalized();
+		_PlayerVelocity.Z = direction.Z * Speed;
+		_PlayerVelocity.X = direction.X * Speed;
+		Rotation = new Vector3(0, Mathf.Atan2(direction.X, direction.Y), 0);
+		if (!IsOnFloor())
+			_PlayerVelocity.Y = -1 * Gravity * (float)delta;
+		Velocity = _PlayerVelocity;
+		MoveAndSlide();
+		public void OnBodyEntered(Node body)
+	{
+		if (body is CharacterBody3D)
+			RemoveLife();
+	}
+	 private void RemoveLife()
+    {
+        var gameManager = GetNode<Var>("/root/player/CanvasLayer/LifeLabel");
+        if (gameManager != null)
+        {
+            gameManager.RemoveLife();
+        }
+        QueueFree();
+}
---
+++ Aula N sei dnv/ZombieIdk.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class ZombieIdk : CharacterBody3D
+{
+		public int Speed = 30;
+		public int Gravity = 196;
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
